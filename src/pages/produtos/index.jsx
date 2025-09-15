b4b035909dae80d8841d72db79cdd03a2c77bcb8
GuilhermeMedeiros24 <rm93782@estudante.fieb.edu.br>
2025-09-15 12:28:46 -0300

    consumo

--- src/pages/produtos/index.jsx
+++ src/pages/produtos/index.jsx
@@ -1,4 +1,27 @@
+import {useState, useEffect} from 'react'
+import axios from 'axios'
+
+
 const Produtos = () => {
+
+    const [dados, setDados] = useStates([]);
+
+    const pegarPizzas = () => {
+
+        axios.get("http://172.19.0.49/pizzariaoficial/api/v1/produto",
+            {'Access-Control-Allow-Origin:*'}
+        )
+        .then((response) => setDados(JSON.stringify(response.data)))
+        .catch((error) => console.log(error))
+    }
+
+    useEffect(()=>{
+        pegarPizzas();
+    }, [])
+
+    useEffect(()=>{
+        console.log(dados)
+    }, [dados])
 
     // Objeto de produtos da lista
     const pizzas = [
