54ebb8849326c02cdebce7efdb426852bf535334
GuilhermeMedeiros24 <rm93782@estudante.fieb.edu.br>
2025-09-15 12:03:27 -0300

    listagem

--- src/App.jsx
+++ src/App.jsx
@@ -1,37 +1,16 @@
-import {useState} from 'react'
+import './App.css'
+import Produtos from './pages/produtos'
 
-function App(){
-
-  const [nome, setNome] = useState('Ronaldo');
-
-  const Formulario = (props) => {
-    return (
-      <>
-        <h4>Formulário de Inscrição</h4>
-        <input className="Nome"
-            name = "nome"
-            placeholder="Digite seu nome..."
-            onChange={(e)=>props.nome = e.target.value}
-            type="text" />
-        <button onClick={()=>{
-        (props.texo != null) ? alert(props.texto) : alert(props.nome)
-          }}>
-          CLIQUE AQUI
-        </button>
-      </>
-    )
-  }
-
+const App = () => {
+  
   return (
     <div>
-      <h3>Pizzaria 2A</h3>
-      <Formulario texto= "Texto enviado pelo App" />
-      <Formulario texto= "São Paulo meu amor" />
-      <Formulario texto= "Novo texto enviado pelo App" />
-      <Formulario nome={nome} />
+      <h1>ProjetoPizzaria</h1>
+      
+      <Produtos />
+
     </div>
   )
 }
 
 export default App
- 
